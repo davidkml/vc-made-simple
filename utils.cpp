14553ee8e4f5a2010038253974d3f58676d7f33b
David Lee <davkmlee@gmail.com>
2019-05-17 11:01:51 -0700

    Implement function for creating directories

--- utils.cpp
+++ utils.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include "utils.h"
+
+using namespace std;
+
+bool is_valid_path(const char* path) {
+    const char* root_dir = ".vms";
+    if (strncmp(root_dir, path, strlen(root_dir)) != 0) {
+        return false;
+    }
+    return true;
+}
+
+int make_dir(const char* dirpath) {
+    if (dirpath == NULL) {
+        cerr << "ERROR: Unable to create directory. Provided name is not a valid string." << endl;
+        return 1;
+    }
+
+    if (!is_valid_path(dirpath)) {
+        cerr << "ERROR: Unable to create directory. Provided name is not a valid path within .vms directory." << endl;
+        return 1;
+    }
+
+    int ret = mkdir(dirpath, 0755);
+
+    if (ret == -1) {
+        cerr << "ERROR: Unable to create directory. " << strerror(errno) << endl;
+        return -1;   
+    }
+
+    return 0;
+}
--- utils.h
+++ utils.h
@@ -1,4 +1,15 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/* 
+    Utility function to create a new directory named dirpath.
+    May only create directories with .vms as the prefix.
+    Returns 0 on success, or non-zero integer error code on 
+    failure.
+
+    Examples: .vms, .vms/objects, .vms/objects/o6
+*/
+int make_dir(const char* dirpath);
+
+
 #endif // UTILS_H
