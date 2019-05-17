3bf473c346d839120a6801bac03c3882b4863551
David Lee <davkmlee@gmail.com>
2019-05-17 11:47:15 -0700

    Remove unnecessary prototype function 'create_empty_file()'

--- utils.h
+++ utils.h
@@ -23,12 +23,4 @@ int make_dir(const char* dirpath);
 */
 int create_and_write_file(const char* filepath, const char* content, mode_t mode);
 
-/* 
-    Utility function to create (or overwrite if already exists) an empty file named filepath with provided permissions.
-    May only create files with .vms as the prefix.
-    Returns 0 on success, or non-zero integer error code on failure.
-
-    Examples: .vms/index, .vms/branches/master
-*/
-int create_empty_file(const char* filepath, mode_t mode);
 #endif // UTILS_H
