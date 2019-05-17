6ee3603475948f12f0d5bade5545034a6893ef5d
David Lee <davkmlee@gmail.com>
2019-05-17 11:32:25 -0700

    Implement function for creating, writing, and setting permissions to file (or overwriting if file already exists)

--- utils.cpp
+++ utils.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <sys/stat.h> // for mkdir
+#include <sys/types.h> // for mkdir, creat
+#include <sys/stat.h> // for creat
+#include <fcntl.h> // for creat
+#include <unistd.h> // for write
 
 #include "utils.h"
 
@@ -32,5 +35,36 @@ int make_dir(const char* dirpath) {
         return -1;   
     }
 
+    return 0;
+}
+
+int create_and_write_file(const char* filepath, const char* content, mode_t mode) {
+    if (filepath == NULL) {
+        cerr << "ERROR: Unable to create file. Provided name is not a valid string." << endl;
+        return 1;
+    }
+
+    if (!is_valid_path(filepath)) {
+        cerr << "ERROR: Unable to create file. Provided name is not a valid path within .vms directory." << endl;
+        return 1;
+    }
+
+    int ofd = creat(filepath, mode);
+    
+    if (ofd == -1) {
+        cerr << "ERROR: Unable to open/create file. " << strerror(errno) << endl;
+        close(ofd);
+        return -1;
+    }
+
+    ssize_t nwrite = write(ofd, content, strlen(content));
+
+    if(nwrite == -1) {
+        cerr << "ERROR: Unable to write to file. " << strerror(errno) << endl;
+        close(ofd);
+        return -1;
+    }
+
+    close(ofd);
     return 0;
 }
--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <sys/stat.h>
+
 /* 
     Utility function to create a new directory named dirpath.
     May only create directories with .vms as the prefix.
@@ -11,5 +13,13 @@
 */
 int make_dir(const char* dirpath);
 
+/* 
+    Utility function to create (or overwrite if already exists) a new file named filepath with provided contents and permissions.
+    May only create files with .vms as the prefix.
+    Returns 0 on success, or non-zero integer error code on failure.
+
+    Examples: .vms/index, .vms/branches/master
+*/
+int create_and_write_file(const char* filepath, const char* content, mode_t mode);
 
 #endif // UTILS_H
