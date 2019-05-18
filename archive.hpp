687e61ac5bc6b260b595e8cc65d548437f5524f0
David Lee <davkmlee@gmail.com>
2019-05-17 18:40:43 -0700

    Create header file and implement functions for serializing and deserializing objects with zlib compression

---
+++ archive.hpp
@@ -0,0 +1,53 @@
+#ifndef ARCHIVE_HPP
+#define ARCHIVE_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
+#include <boost/iostreams/filter/zlib.hpp>
+#include <boost/iostreams/filtering_streambuf.hpp>
+
+
+template <class T>
+void save(T& obj, std::string filepath) {
+    std::ofstream ofs(filepath);
+    if (!ofs.is_open()) {
+        std::cerr << "ERROR: File could not be opened." << std::endl;
+        return;
+    }
+
+    {
+        boost::iostreams::filtering_ostreambuf fos_buf;
+
+        fos_buf.push(boost::iostreams::zlib_compressor(boost::iostreams::zlib::best_compression));
+        fos_buf.push(ofs);
+
+        boost::archive::binary_oarchive boa(fos_buf);
+        boa << obj;
+    }
+}
+
+template <class T>
+void restore(T& obj, std::string filepath) {
+    std::ifstream ifs(filepath);
+    if (!ifs.is_open()) {
+        std::cerr << "ERROR: File could not be opened." << std::endl;
+        return;
+    }
+
+    {
+        boost::iostreams::filtering_istreambuf fis_buf;
+
+        fis_buf.push(boost::iostreams::zlib_decompressor());
+        fis_buf.push(ifs);
+
+        boost::archive::binary_iarchive bia(fis_buf);
+        bia >> obj;
+    }
+}
+
+#endif // ARCHIVE_HPP
